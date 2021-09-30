25df777881f4651990f8077b7a04e9b6eedddbc5
Jay Lord P. Torres <isens.jaylord@gmail.com>
2021-09-30 20:18:55 +0800

    ple style on footer
    Signed-off-by: Jay Lord P. Torres <isens.jaylord@gmail.com>

--- src/shared/component/footer.js
+++ src/shared/component/footer.js
@@ -1,5 +1,14 @@
-import { Typography } from "@mui/material";
+import { Divider, Typography } from "@mui/material";
+import { useFooterStyle } from "../style/footer.style";
 
 export const Footer = () => {
-  return <Typography variant="h5">footer</Typography>;
+  const classes = useFooterStyle();
+
+  return (
+    <div className={classes.container}>
+      <Typography variant="h5">footer</Typography>
+      <Divider />
+      <Typography classes={classes.buy}>buy</Typography>
+    </div>
+  );
 };
---
+++ src/shared/style/footer.style.js
@@ -0,0 +1,10 @@
+import { makeStyles } from "@mui/styles";
+
+export const useFooterStyle = makeStyles((theme) => ({
+  container: {
+    padding: theme.spacing(2),
+  },
+  buy: {
+    color: "red",
+  },
+}));
