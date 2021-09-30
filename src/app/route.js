24472a4050d7efa4a16a4fe3deec9ecf923885a4
Jay Lord P. Torres <isens.jaylord@gmail.com>
2021-09-30 19:46:05 +0800

    home init

--- src/app/route.js
+++ src/app/route.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { AuthPage } from "../auth/page/auth";
 import { DashboardPage } from "../dashboard/page/dashboard";
+import { HomePage } from "../home/page/home";
 
 export const Pages = () => {
   return (
@@ -8,6 +9,7 @@ export const Pages = () => {
       <Switch>
         <Route path={DashboardPage.path} component={DashboardPage} />
         <Route path={AuthPage.path} component={AuthPage} />
+        <Route path={HomePage.path} component={HomePage} />
       </Switch>
     </BrowserRouter>
   );
