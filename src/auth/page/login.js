7143be56c17ea3c876af4a7e9200715e6b11c2dd
Jay Lord P. Torres <isens.jaylord@gmail.com>
2021-09-30 20:31:26 +0800

    sample routing

--- src/auth/page/login.js
+++ src/auth/page/login.js
@@ -1,3 +1,18 @@
-export const LoginPage = () => {
-  return "loginPage";
+import { useCallback } from "react";
+import { Button, Typography } from "@mui/material";
+
+// using global paths as sample
+import { AppPaths } from "../../app/gobal";
+
+export const LoginPage = ({ history }) => {
+  const onRegister = useCallback(() => {
+    history.push(AppPaths.auth.register);
+  }, [history]);
+
+  return (
+    <div>
+      <Typography variant="h1">Login</Typography>
+      <Button onClick={onRegister}>redir to register using callback</Button>
+    </div>
+  );
 };
--- src/auth/page/register.js
+++ src/auth/page/register.js
@@ -1,3 +1,17 @@
+import { Button, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+
+// using local path as sample
+import { AuthPaths } from "../path";
+
 export const RegisterPage = () => {
-  return "register page";
+  return (
+    <div>
+      <Typography variant="h1">Register</Typography>
+
+      <Link component={Button} to={AuthPaths.login}>
+        redir to login using Link
+      </Link>
+    </div>
+  );
 };
